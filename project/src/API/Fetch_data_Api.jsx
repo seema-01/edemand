fddec6f7495c5f6a2bec79b5d79e54e0561d4d46
Dhruv Soni <dhruvrsoni04@gmail.com>
2023-04-12 19:37:34 +0530

    creating api call

--- project/src/API/Fetch_data_Api.jsx
+++ project/src/API/Fetch_data_Api.jsx
@@ -1,4 +1,4 @@
- async function get_Api_Category() {
+async function get_Api_Category() {
     var formdata = new FormData();
     formdata.append("latitude", "23.2507356");
     formdata.append("longitude", "69.6689201");
@@ -10,14 +10,32 @@
     };
 
     fetch("https://edemand.wrteam.me/api/v1/get_categories", requestOptions)
-    .then(response => response.json())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+        .then(response => response.json())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
 
 }
 
- async function get_categories(){
-    console.log("hi")
+async function get_sub_categories_home() {
+    var myHeaders = new Headers();
+    myHeaders.append("Cookie", "ci_session=6mnmahtt4h48idvd0et8iji2ehl112e4; csrf_cookie_name=00efa4096090fa5a82619f221434628f");
+
+    var formdata = new FormData();
+    formdata.append("latitude", "23.2507356");
+    formdata.append("longitude", "69.6689201");
+    formdata.append("category_id", "213");
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: formdata,
+        redirect: 'follow'
+    };
+
+    fetch("https://edemand.wrteam.me/api/v1/get_sub_categories", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
 }
 
-export default {get_Api_Category , get_categories }
+export default { get_Api_Category, get_sub_categories_home }
