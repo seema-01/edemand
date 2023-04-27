e4b8dfe3c978e2a657b8f39649cbf0e33cf487df
Dhruv Soni <dhruvrsoni04@gmail.com>
2023-04-27 20:59:24 +0530

    Profile Completed

--- project/src/Components/Reusable/Profile/ProfileNotification.jsx
+++ project/src/Components/Reusable/Profile/ProfileNotification.jsx
@@ -24,48 +24,42 @@ const ProfileNotification = () => {
             <Box sx={{ mt: 3, background: "white", borderRadius: "10px" }}>
               <Heading heading="Notifications" />
               <Box>
-                <List
-                  sx={{
-                    maxWidth: "100%",
-                    bgcolor: "background.paper",
-                  }}
-                >
-                  <ListItem>
-                    <ListItemAvatar>
-                      <Avatar>
-                        <ImageIcon />
-                      </Avatar>
-                    </ListItemAvatar>
-                    <ListItemText primary="Photos" secondary="Jan 9, 2014" />
-                  </ListItem>
-                  <Divider variant="inset" component="li" />
-                  <ListItem>
-                    <ListItemAvatar>
-                      <Avatar>
-                        <WorkIcon />
-                      </Avatar>
-                    </ListItemAvatar>
-                    <ListItemText primary="Work" secondary="Jan 7, 2014" />
-                  </ListItem>
-                  <Divider variant="inset" component="li" />
-                  <ListItem>
-                    <ListItemAvatar>
-                      <Avatar>
-                        <BeachAccessIcon />
-                      </Avatar>
-                    </ListItemAvatar>
-                    <ListItemText
-                      primary="Vacation"
-                      secondary="July 20, 2014"
-                    />
-                  </ListItem>
-                </List>
+                <NotificationList />
+                <NotificationList />
+                <NotificationList />
+                <NotificationList />
+                <NotificationList />
+                <NotificationList />
               </Box>
             </Box>
           </Grid>
         </Grid>
       </Container>
     </div>
+  );
+};
+
+export const NotificationList = () => {
+  return (
+    <>
+      <List
+        sx={{
+          maxWidth: "100%",
+          bgcolor: "background.paper",
+          padding: 0
+        }}
+      >
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar>
+              <ImageIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="Order Completed" secondary="Jan 9, 2014" />
+        </ListItem>
+        <Divider variant="inset" component="li" />
+      </List>
+    </>
   );
 };
 
