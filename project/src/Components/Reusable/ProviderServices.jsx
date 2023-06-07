4c958b1a750c716df59d54d2aa68ca7f53f0af7d
Dhruv Soni <dhruvrsoni04@gmail.com>
2023-06-07 07:36:05 +0530

    Final Commit

--- project/src/Components/Reusable/ProviderServices.jsx
+++ project/src/Components/Reusable/ProviderServices.jsx
@@ -35,8 +35,9 @@ const ProviderServices = ({ match }) => {
 
   const [open, setOpen] = React.useState(false);
   const [data, setData] = useState([]);
+  const [provider, setProvider] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-
+  const [parentPicture, setParentPicture] = useState("");
 
   const Data = useSelector((state) => state.cart);
   const dispatch = useDispatch();
@@ -56,11 +57,12 @@ const ProviderServices = ({ match }) => {
     localStorage.setItem("cart", JSON.stringify(cartData));
   };
 
-
   //dynamic urlN
   const params = useParams();
   const { partner_id } = params;
-  console.log("partner_id" +partner_id)
+  console.log("partner_id" + partner_id);
+
+  let finalDoc = 0;
 
   async function allData() {
     var formdata = new FormData();
@@ -80,12 +82,19 @@ const ProviderServices = ({ match }) => {
     );
     const result = await response.json();
     console.log(result);
+    setParentPicture(partner_id);
+    console.log("Stored ID :" + parentPicture);
     return result;
   }
 
   useEffect(() => {
     allData()
       .then((response) => setData(response.data))
+      .then((response) => setIsLoading(true))
+      .then((response) => console.log(response));
+    api
+      .get_providers()
+      .then((response) => setProvider(response.data))
       .then((response) => setIsLoading(true))
       .then((response) => console.log(response));
   }, []);
@@ -136,92 +145,90 @@ const ProviderServices = ({ match }) => {
                   {isLoading ? (
                     <Box>
                       {data.map((response) => {
-                          return (
-                            <Box sx={{ flexGrow: 1 }}>
-                              <Card
-                                sx={{
-                                  display: "flex",
-                                  boxShadow: "none",
-                                  height: 200,
-                                  p: 1,
-                                }}
-                              >
-                                <Grid container display={"flex"} spacing={3}>
-                                  <Grid item md={4}>
-                                    <CardMedia
-                                      image={response.image_of_the_service}
-                                      alt="hi"
+                        return (
+                          <Box sx={{ flexGrow: 1 }}>
+                            <Card
+                              sx={{
+                                display: "flex",
+                                boxShadow: "none",
+                                height: 200,
+                                p: 1,
+                              }}
+                            >
+                              <Grid container display={"flex"} spacing={3}>
+                                <Grid item md={4}>
+                                  <CardMedia
+                                    image={response.image_of_the_service}
+                                    alt="hi"
+                                    sx={{
+                                      height: "180px",
+                                      width: "200px",
+                                      borderRadius: "4px",
+                                    }}
+                                  />
+                                </Grid>
+                                <Grid item md={8}>
+                                  <CardContent sx={{ ml: 2, p: 0 }}>
+                                    <Box
                                       sx={{
-                                        height: "180px",
-                                        width: "200px",
-                                        borderRadius: "4px",
+                                        display: "block",
+                                        textAlign: "start",
                                       }}
-                                    />
-                                  </Grid>
-                                  <Grid item md={8}>
-                                    <CardContent sx={{ ml: 2, p: 0 }}>
-                                      <Box
-                                        sx={{
-                                          display: "block",
-                                          textAlign: "start",
-                                        }}
-                                      >
-                                        <Box sx={{ display: "flex" }}>
-                                          <Typography
-                                            color={"#2560FC"}
-                                            sx={{ marginRight: "auto" }}
-                                          >
-                                            {response.title}
-                                          </Typography>
-                                          <StarIcon sx={{ color: "gold" }} />
-                                          {response.rating}
-                                          <br />
-                                        </Box>
-                                        <Typography fontSize={14} pt={2}>
-                                          {response.description}
-                                        </Typography>
-                                        <br /> <br />
+                                    >
+                                      <Box sx={{ display: "flex" }}>
                                         <Typography
-                                          color={"gray"}
-                                          fontSize={14}
-                                          sx={{ mt: -4 }}
+                                          color={"#2560FC"}
+                                          sx={{ marginRight: "auto" }}
                                         >
-                                          2 Person | 150 min
+                                          {response.title}
                                         </Typography>
+                                        <StarIcon sx={{ color: "gold" }} />
+                                        {response.rating}
                                         <br />
-                                        <Box sx={{ display: "flex", mt: 3 }}>
-                                          <Typography
-                                            color={"#2560FC"}
-                                            sx={{ marginRight: "auto" }}
-                                          >
-                                            ${response.discounted_price}
-                                            <del style={{ color: "gray" }}>
-                                              ${response.price}
-                                            </del>
-                                          </Typography>
+                                      </Box>
+                                      <Typography fontSize={14} pt={2}>
+                                        {response.description}
+                                      </Typography>
+                                      <br /> <br />
+                                      <Typography
+                                        color={"gray"}
+                                        fontSize={14}
+                                        sx={{ mt: -4 }}
+                                      >
+                                        2 Person | 150 min
+                                      </Typography>
+                                      <br />
+                                      <Box sx={{ display: "flex", mt: 3 }}>
+                                        <Typography
+                                          color={"#2560FC"}
+                                          sx={{ marginRight: "auto" }}
+                                        >
+                                          ${response.discounted_price}
+                                          <del style={{ color: "gray" }}>
+                                            ${response.price}
+                                          </del>
+                                        </Typography>
 
-                                          <Box sx={{ float: "right", mt: -1 }}>
-                                            <Button
-                                              variant="outlined"
-                                              onClick={() =>
-                                                handleOpen(response)
-                                              }
-                                              float="right"
-                                              size="small"
-                                            >
-                                              Add
-                                            </Button>
-                                            <ToastContainer autoClose={3000} />
-                                          </Box>
+                                        <Box sx={{ float: "right", mt: -1 }}>
+                                          <Button
+                                            variant="outlined"
+                                            onClick={() => handleOpen(response)}
+                                            float="right"
+                                            size="small"
+                                          >
+                                            Add
+                                          </Button>
+                                          <ToastContainer autoClose={3000} />
                                         </Box>
                                       </Box>
-                                    </CardContent>
-                                  </Grid>
+                                    </Box>
+                                  </CardContent>
                                 </Grid>
-                              </Card>
-                              <Divider />
-                            </Box>
-                          );
+                              </Grid>
+                            </Card>
+                            <Divider />
+                          </Box>
+                        );
                       })}
                     </Box>
                   ) : (
@@ -271,82 +278,129 @@ const ProviderServices = ({ match }) => {
           <Grid item xs={12} md={5}>
             <Box>
               <Card sx={{}}>
-                <CardMedia
-                  sx={{ height: 250 }}
-                  image="https://picsum.photos/300/300"
-                  title="green iguana"
-                />
-                <CardMedia
-                  sx={{
-                    height: 80,
-                    width: 80,
-                    borderRadius: 50,
-                    ml: "auto",
-                    mr: "auto",
-                    border: "5px solid white",
-                    mt: -5,
-                  }}
-                  image="https://picsum.photos/300/300"
-                  title="green iguana"
-                />
-                <CardActions sx={{ justifyContent: "space-around", mb: 4 }}>
-                  <Button
-                    variant="contained"
-                    sx={{ textAlign: "start", fontSize: 15 }}
-                  >
-                    <Avatar sx={{ bgcolor: "white" }}>
-                      <CheckCircleIcon
-                        sx={{
-                          bgcolor: "white",
-                          color: "blue",
-                          borderRadius: "50px",
-                        }}
-                      />
-                    </Avatar>{" "}
-                    <Box sx={{ ml: 2 }}>
-                      <h3>89</h3>{" "}
-                      <p style={{ fontSize: 10 }}>Orders Complete</p>
-                    </Box>
-                  </Button>
-                  <Button
-                    variant="contained"
-                    sx={{
-                      textAlign: "start",
-                      fontSize: 15,
-                      bgcolor: "#323232",
-                      "&:hover": { bgcolor: "#323232" },
-                    }}
-                  >
-                    <Avatar sx={{ bgcolor: "white" }}>
-                      <StarIcon sx={{ color: "gray", borderRadius: "50px" }} />
-                    </Avatar>{" "}
-                    <Box sx={{ ml: 2 }}>
-                      <h3>4.3</h3>{" "}
-                      <p style={{ fontSize: 10 }}>provider rating</p>
-                    </Box>
-                  </Button>
-                </CardActions>
+                {isLoading ? (
+                  <Box>
+                    {provider.map((response) => {
+                      return (
+                        <>
+                          {response.partner_id === partner_id ? (
+                            <>
+                              <CardMedia
+                                sx={{ height: 250 }}
+                                image={response.banner_image}
+                                title="green iguana"
+                              />
+                              <CardMedia
+                                sx={{
+                                  height: 80,
+                                  width: 80,
+                                  borderRadius: 50,
+                                  ml: "auto",
+                                  mr: "auto",
+                                  border: "5px solid white",
+                                  mt: -5,
+                                }}
+                                image={response.image}
+                                title="green iguana"
+                              />
+                              <CardActions
+                                sx={{ justifyContent: "space-around", mb: 4 }}
+                              >
+                                <Button
+                                  variant="contained"
+                                  sx={{ textAlign: "start", fontSize: 15 }}
+                                >
+                                  <Avatar sx={{ bgcolor: "white" }}>
+                                    <CheckCircleIcon
+                                      sx={{
+                                        bgcolor: "white",
+                                        color: "blue",
+                                        borderRadius: "50px",
+                                      }}
+                                    />
+                                  </Avatar>
+                                  <Box sx={{ ml: 2 }}>
+                                    <h3>{response.number_of_orders}</h3>
+                                    <p style={{ fontSize: 10 }}>
+                                      Orders Complete
+                                    </p>
+                                  </Box>
+                                </Button>
+                                <Button
+                                  variant="contained"
+                                  sx={{
+                                    textAlign: "start",
+                                    fontSize: 15,
+                                    bgcolor: "#323232",
+                                    "&:hover": { bgcolor: "#323232" },
+                                  }}
+                                >
+                                  <Avatar sx={{ bgcolor: "white" }}>
+                                    <StarIcon
+                                      sx={{
+                                        color: "gray",
+                                        borderRadius: "50px",
+                                      }}
+                                    />
+                                  </Avatar>
+                                  <Box sx={{ ml: 2 }}>
+                                    <h3>{response.ratings}</h3>
+                                    <p style={{ fontSize: 10 }}>
+                                      provider rating
+                                    </p>
+                                  </Box>
+                                </Button>
+                              </CardActions>
 
-                <hr />
+                              <hr />
 
-                <CardContent>
-                  <Typography gutterBottom variant="h6" component="div">
-                    <strong> Provider Address </strong>
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Katira Complex, 8/a, Sanskar Nagar, Bhuj, Gujrat <br />
-                    370001
-                  </Typography>
-                  <Typography gutterBottom variant="h6" component="div" mt={2}>
-                    <strong> Provider Information </strong>
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    Nemo dolor inventore quibusdam quas. Expedita voluptate
-                    dolores labore, voluptatem iure repudiandae facilis. Eum
-                    nihil excepturi, ex molestias quas assumenda aut possimus.
-                  </Typography>
-                </CardContent>
+                              <CardContent>
+                                <Typography
+                                  gutterBottom
+                                  variant="h6"
+                                  component="div"
+                                >
+                                  <strong> Provider Address </strong>
+                                </Typography>
+                                <Typography
+                                  variant="body2"
+                                  color="text.secondary"
+                                >
+                                  {response.address}
+                                  <br />
+                                  370001
+                                </Typography>
+                                <Typography
+                                  gutterBottom
+                                  variant="h6"
+                                  component="div"
+                                  mt={2}
+                                >
+                                  <strong> Provider Information </strong>
+                                </Typography>
+                                <Typography
+                                  variant="body2"
+                                  color="text.secondary"
+                                >
+                                  Lorem, ipsum dolor sit amet consectetur
+                                  adipisicing elit. Nemo dolor inventore
+                                  quibusdam quas. Expedita voluptate dolores
+                                  labore, voluptatem iure repudiandae facilis.
+                                  Eum nihil excepturi, ex molestias quas
+                                  assumenda aut possimus.
+                                </Typography>
+                              </CardContent>
+                            </>
+                          ) : (
+                            <></>
+                          )}
+                        </>
+                      );
+                    })}
+                  </Box>
+                ) : (
+                  <Skeleton variant="rectangular" height={"800px"}/>
+                )}
               </Card>
             </Box>
             <Box
